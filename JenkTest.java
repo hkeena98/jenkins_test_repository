b655659961d96559d53777b1f481635cee9091ac
Henry Keena <hkeena98@gmail.com>
2019-05-21 15:29:28 -0400

    Add files via upload

---
+++ JenkTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Henry Keena
+ * This just some simple test code for the Jenkins Multibranch Pipeline. 
+ */
+
+public class JenkTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a simple test of our Jenkins Multibranch Pipeline.");
+    }
+}
